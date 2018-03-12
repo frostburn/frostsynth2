01ca12d5da5b16b1d8903d0e9621b660e1766b6d
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2018-03-12 22:22:46 +0200

    Implement frequency cleaning
    Fix amplitude magnitude bug in frequency decomposition.

--- frostsynth/demos/whistle.py
+++ frostsynth/demos/whistle.py
@@ -5,26 +5,20 @@ from numpy import *
 from scipy.io import wavfile
 
 from . import DATA_PATH
-from ..process import decompose_frequency
+from .. import tau
+from ..process import decompose_frequency, clean_frequency
 from ..filter import Polezero
 from ..sampling import time_like, read_and_set_sample_rate, shift, write
 
 data = read_and_set_sample_rate(os.path.join(DATA_PATH, "whistle.wav"))
 
 frequency, amplitude = decompose_frequency(data)
-
-polezero = Polezero()
-polezero.a1 = -1
-polezero.b0 = 0.02
-polezero.a0 = polezero.b0 - polezero.a1
-polezero.b1 = 0
-frequency = array(polezero.map(frequency))
-amplitude = maximum(0, array(polezero.map(amplitude)) - 0.01)
+frequency, amplitude = clean_frequency(frequency, amplitude)
 
 vibrato = 1 + 0.02 * sin(time_like(frequency) * 5)
 
-phase = cumsum(frequency * vibrato * 0.5)
-data = 2 * sin(phase + sin(5 * phase) * amplitude) * amplitude
+phase = cumsum(frequency * vibrato * 0.5) * tau
+data = sin(phase + sin(5 * phase) * amplitude) * amplitude
 
 filename = raw_input("Enter (WAV) output filename: ")
 if filename:
--- frostsynth/process.py
+++ frostsynth/process.py
@@ -12,10 +12,25 @@ def decompose_frequency(data, window_size=1024):
 
     chunks = map(analysis.hilbert, chunks)
 
-    data = 2 * chunk.dechunkify(chunks, overlap=4)
+    data = 4 * chunk.dechunkify(chunks, overlap=4)
 
     phase = np.unwrap(np.angle(data)) / tau
     frequency = np.concatenate(([0.0], np.diff(phase)))
     amplitude = abs(data)
 
     return frequency, amplitude
+
+
+def clean_frequency(frequency, amplitude, window_size=1024):
+    w = window.cosine(window_size)
+    weighted_frequency = np.convolve(frequency * amplitude, w)
+    average_weight = np.convolve(amplitude, w)
+    return (
+        weighted_frequency / (average_weight + (average_weight == 0)),
+        average_weight / w.sum()
+    )
+
+
+def recompose_frequency(frequency, amplitude):
+    phase = np.cumsum(frequency)
+    return np.sin(tau * phase) * amplitude
