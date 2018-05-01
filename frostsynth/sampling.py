4c2322e34daee61ae0479dfaafc3ab30e6512c0b
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2018-05-01 11:49:14 +0300

    Make sure that stero tracks are written correctly

--- frostsynth/sampling.py
+++ frostsynth/sampling.py
@@ -50,6 +50,16 @@ def read_and_set_sample_rate(filename):
 
 @sampled
 def write(filename, data):
+    if not isinstance(data, np.ndarray):
+        data = np.array(data, dtype=float)
+
+    # Figure out the number of channels
+    shape = data.shape
+    if len(shape) > 3:
+        raise ValueError("Data shape not understood. Not single or multi-channel.")
+    if len(data.shape) > 1 and shape[0] < shape[1]:
+        data = data.T
+
     if data.dtype == float:
         data = (data * (0.99 * 2.0 ** 15)).astype("int16")
     scipy.io.wavfile.write(filename, SAMPLE_RATE, data)
--- test/test_sampling.py
+++ test/test_sampling.py
@@ -1,3 +1,5 @@
+from StringIO import StringIO
+
 import numpy as np
 
 from frostsynth import sampling
@@ -16,3 +18,59 @@ def test_merge():
        0.5, 0.7 , 0.9 , 1.1 , 1.3 , 0.5 , 0.6 , 0.7 , 0.8 , 0.9 ,
        0, 0.01, 0.04, 0.09, 0.16, 0.25, 0.36, 0.49, 0.64, 0.81
     ])
+
+
+def test_write_mono():
+    sampling.set_sample_rate(10)
+    t = sampling.trange(1)
+    signal = np.sin(t)
+    sio = StringIO()
+    sampling.write(sio, signal)
+    assert (sio.getvalue() == ''
+        'RIFF8\x00\x00\x00WAVEfmt '
+        '\x10\x00\x00\x00\x01\x00\x01\x00\n\x00\x00\x00\x14\x00\x00\x00\x02\x00\x10\x00'
+        'data\x14\x00\x00\x00\x00\x00\xa6\x0c,\x19r%X1\xc0<\x8dG\xa2Q\xe7ZCc'
+    )
+
+
+def test_write_stereo():
+    sampling.set_sample_rate(10)
+    t = sampling.trange(1)
+    left = np.sin(t)
+    right = np.cos(t)
+    sio = StringIO()
+    sampling.write(sio, [left, right])
+    assert (sio.getvalue() == ''
+        'RIFFL\x00\x00\x00WAVEfmt '
+        '\x10\x00\x00\x00\x01\x00\x02\x00\n\x00\x00\x00(\x00\x00\x00\x04\x00\x10\x00'
+        'data(\x00\x00\x00\x00\x00\xb8~\xa6\x0c\x16~,\x191|r%\x0fyX1\xb7t\xc0<5o\x8dG\x96h\xa2Q\xeb`\xe7ZIXCc\xc5N'
+    )
+
+
+def test_read_mono():
+    sio = StringIO()
+    sio.write(''
+        'RIFF8\x00\x00\x00WAVEfmt '
+        '\x10\x00\x00\x00\x01\x00\x01\x00\n\x00\x00\x00\x14\x00\x00\x00\x02\x00\x10\x00'
+        'data\x14\x00\x00\x00\x00\x00\xa6\x0c,\x19r%X1\xc0<\x8dG\xa2Q\xe7ZCc'
+    )
+    sio.seek(0)
+    signal = sampling.read_and_set_sample_rate(sio)
+    t = sampling.trange(1)
+    assert (sampling.get_sample_rate() == 10)
+    assert np.allclose(np.sin(t), signal, 0.01, 0.01)
+
+
+def test_read_stereo():
+    sio = StringIO()
+    sio.write(''
+        'RIFFL\x00\x00\x00WAVEfmt '
+        '\x10\x00\x00\x00\x01\x00\x02\x00\n\x00\x00\x00(\x00\x00\x00\x04\x00\x10\x00'
+        'data(\x00\x00\x00\x00\x00\xb8~\xa6\x0c\x16~,\x191|r%\x0fyX1\xb7t\xc0<5o\x8dG\x96h\xa2Q\xeb`\xe7ZIXCc\xc5N'
+    )
+    sio.seek(0)
+    left, right = sampling.read_and_set_sample_rate(sio)
+    t = sampling.trange(1)
+    assert (sampling.get_sample_rate() == 10)
+    assert np.allclose(np.sin(t), left, 0.01, 0.01)
+    assert np.allclose(np.cos(t), right, 0.01, 0.01)
